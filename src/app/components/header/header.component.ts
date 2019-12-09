accc2b569ba74ba4e5a2d247d8be433f46deb2c6
guzhongren <guzhongren@live.cn>
2019-12-09 22:18:41 +0800

    feat(pipe,dect,mvvm): 学习管道，指令、mvvm,事件，方法

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  testTitle: string = "test Header";
+
+  arrs: string[] = ["1", '2', '3', '4']
+  arrObj = [{
+    name: 'test',
+    age: 12,
+  }, {
+    name: 'lisi',
+    age: 23,
+  }];
+  cars = [
+  {
+    cate: '宝马',
+    list: [
+      {
+        title: "宝马1",
+        price: 30,
+      },
+      {
+        title: "宝马2",
+        price: 33,
+      },
+      {
+        title: "宝马4",
+        price: 40,
+      },
+    ]
+  },
+  {
+    cate: '奥迪',
+    list: [
+      {
+        title: "奥迪1",
+        price: 30,
+      },
+      {
+        title: "奥迪2",
+        price: 33,
+      },
+      {
+        title: "奥迪4",
+        price: 40,
+      },
+    ]
+  },
+
+  ]
+  today = new Date()
+  constructor() { }
+
+
+  ngOnInit() {
+  }
+  click() {
+    this.testTitle = '改变后的数据'
+  }
+  handleInput(evt) {
+    if (evt.keyCode === 13) {
+      this.testTitle = evt.target.value
+    } else {
+      console.log(evt)
+    }
+
+  }
+
+}
