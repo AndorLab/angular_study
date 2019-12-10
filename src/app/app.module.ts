0cad34c455bc9aec244e8a38015026c505869e86
guzhongren <guzhongren@live.cn>
2019-12-10 22:56:17 +0800

    feat(TodoList): 新增todolist 案例

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,15 @@ import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component'
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    TodoListComponent,
   ],
   imports: [
     BrowserModule,
