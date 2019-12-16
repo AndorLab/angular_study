526db907bf96f34611f70c8c7bff875ab09cb8ec
guzhongren <guzhongren@live.cn>
2019-12-16 23:04:50 +0800

    feat(router): 新增路由

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { HomeComponent } from './components/home/home.component';
-import { TodoListComponent } from './components/todo-list/todo-list.component';
-import { HttpClientModule } from '@angular/common/http';
-
-import { StorageService } from './service/storage.service';
-import { NewsComponent } from './components/news/news.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent,
-    TodoListComponent,
-    NewsComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpClientModule,
+    AppRoutingModule
   ],
-  providers: [
-    StorageService,
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
